f33a519bc91e9881f5ba1422addbedd33e1c7ad4
Brent Vatne <brentvatne@gmail.com>
2016-12-08 17:51:56 -0800

    RIP

---
+++ index.js
@@ -0,0 +1,1 @@
+console.error("react-native-modal is no longer recommended, see the README for details: https://github.com/brentvatne/react-native-modal");
---
+++ warn.js
@@ -0,0 +1,6 @@
+console.warn("*****************************************************************")
+console.warn("react-native-modal has been sunset! Do not use this package.");
+console.warn("");
+console.warn("There are better options available. See the README for details.");
+console.warn("https://github.com/brentvatne/react-native-modal");
+console.warn("****************************************************************")
